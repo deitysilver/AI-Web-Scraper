08469768fe46714e555ccd70365e4b1b0897ed06
Silver <166840363+theonesilverest@users.noreply.github.com>
2024-09-18 21:29:52 +0530

    Version 1.1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit
 import requests
 from bs4 import BeautifulSoup
 
-genai.configure(api_key="API_KEY")
+genai.configure(api_key="AIzaSyDkq4fHABnqc4ck7GALecxbnHCwJ-WT3_M")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 def generate(data):
@@ -12,7 +12,7 @@ def generate(data):
 def scrape(url):
     response = requests.get(url)
     response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, "html.parser")
 
     return soup.get_text()
 
@@ -20,22 +20,26 @@ streamlit.title("Web Scraper")
 site_url = streamlit.text_input("Enter Website URL: ")
 
 if site_url:
-    streamlit.divider()
     if not site_url.startswith(("http://", "https://")):
         site_url = "http://" + site_url
 
     try:
+        streamlit.divider()
         streamlit.subheader("Site Description")
         site_content = scrape(site_url)
-        
-        expander = streamlit.expander("Scraped Content:")
-        expander.write(site_content)
 
+        streamlit.write(generate("This is the url: " + site_url + "\n This is the data: \n" + site_content + "\n Can you give me a brief description of this content? 2 sentences maximium."))        
+        expander = streamlit.expander("Raw Scraped Content")
+        expander.write(site_content)
+        streamlit.divider()
+        
+
+        streamlit.subheader("Chat With Your Data")
         prompt = streamlit.text_input("Enter Prompt: ")
 
         if streamlit.button("Ask Anything"):
             streamlit.divider()
-            response = generate("/n This is the data: /n" + site_content + "/n And this is the prompt" + prompt)
+            response = generate("\n This is the data: \n" + site_content + "\n And this is the prompt" + prompt)
             streamlit.write(response)
 
     except Exception as e:
